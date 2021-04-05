95429c0d60650e951abee6820ae430c03dd9481b
duke <>
2021-04-05 20:49:20 +0800

    完成两两交换链表中的节点(递归解法&迭代解法)

--- src/_00_leetcode/链表/_24_两两交换链表中的节点.java
+++ src/_00_leetcode/链表/_24_两两交换链表中的节点.java
@@ -16,21 +16,41 @@ import java.util.Queue;
 public class _24_两两交换链表中的节点 {
     /**
      * 官方：递归解法
+     * 参考：https://lyl0724.github.io/2020/01/25/1/
      * @param head
      * @return
      */
-    public static ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
+    public static ListNode swapPairs1(ListNode head) {
+        if (head == null || head.next == null) return head; // 终止条件：链表只剩一个节点或者没节点了，没得交换了。返回的是已经处理好的链表
+        // [1,2,3,4]
+        // 一共三个节点:head, next, swapPairs(next.next)
+        // 下面的任务便是交换这3个节点中的前两个节点
+        ListNode next = head.next;
+        head.next = swapPairs1(next.next); // 将其余节点进行两两交换 (当前节点的next为交换后的头结点)
+        next.next = head; // 本级递归操作
+        return next; // 根据第二步：返回给上一级的是当前已经完成交换后，即处理好了的链表部分
+    }
 
-        // 思路：
-        // 如果链表中至少有两个节点，则在两两交换链表中的节点之后:
-        // 原始链表的头节点变成新的链表的第二个节点，原始链表的第二个节点变成新的链表的头节点。
-        // 链表中的其余节点的两两交换可以递归地实现。在对链表中的其余节点递归地两两交换之后，
-        // 更新节点之间的指针关系，即可完成整个链表的两两交换。
-        ListNode newHead = head.next; // newHead 表示新的链表的头节点 是原链表的下一个节点
-        head.next = swapPairs(newHead.next); // 表示将其余节点进行两两交换 (其余新链表的头节点是原链表的下一个)
-        //newHead.next = head; // 交换后的 新的头节点 为 head 的下一个节点
-        return newHead; // 最后返回 新的链表的头节点 newHead。
+    /**
+     * 官方：迭代
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs2(ListNode head) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode temp = dummyNode; // temp 表示当前到达的节点
+        // [1,2,3,4]
+        while (temp.next != null && temp.next.next != null) { // 只有当temp指向的结点之后有两个结点时才需要交换
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2; // 衔接
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1; // temp指向交换后的结点的第二个结点，即未交换结点的前一个结点
+        }
+
+        return dummyNode.next;
     }
 
     public static void main(String[] args) {
@@ -39,7 +59,7 @@ public class _24_两两交换链表中的节点 {
         ListNode node3 = new ListNode(2, node2);
         ListNode node4 = new ListNode(1, node3);
 
-        ListNode head = swapPairs(node4);
+        ListNode head = swapPairs2(node4);
         head.printList(head);
     }
 }
